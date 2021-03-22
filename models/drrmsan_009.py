653dd8d1d205929522ab3a52c9d230f887e59004
Jimut123 <jimutbahanpal@yahoo.com>
2021-03-22 20:13:16 +0530

    drrmsan added

--- models/drrmsan_009.py
+++ models/drrmsan_009.py
@@ -240,8 +240,8 @@ def proposed_attention_block_2d(ms_conv, res_block, filters):
     output_ms_conv_res_block_rb = multiply([resampler_down_upper_rb, resampler_down_lower_rb])
     
     attn_outputs_mult = Activation('sigmoid')(multiply([output_ms_conv_res_block, output_ms_conv_res_block_rb]))
-    attn_output_1 = UpSampling2D(size=(4, 4))(attn_outputs_mult)
-    # attn_output = multiply([attn_output_1, theta_x_rb])
+    attn_output_1 = UpSampling2D(size=(4, 4), strides=(2, 2))(attn_outputs_mult)
+    attn_output = multiply([attn_output_1, theta_x_rb])
     return attn_output_1
 
 
@@ -447,6 +447,7 @@ def DRRMSAN_multiscale_attention_bayes_009(height, width, n_channels, alpha_1, a
     #up6_add =  add([Conv2DTranspose(32*8, (2, 2), strides=(2, 2), padding='same')(mresblock5), mresblock4])
     #up6_dra = attention_up_and_concate(Conv2DTranspose(32*8, (2, 2), strides=(2, 2), padding='same', name='up6_dra')(mresblock5), mresblock4,filters=32*8)
     up6 = proposed_attention_block_2d(Conv2DTranspose(32*8, (2, 2), strides=(2, 2), padding='same', name='up6')(mresblock5), mresblock4,filters=64)
+    
     up6 = add([up6, mresblock4])
     
     #concatenate([Conv2DTranspose(32*8, (2, 2), strides=(2, 2), padding='same')(mresblock5), mresblock4], axis=3)
