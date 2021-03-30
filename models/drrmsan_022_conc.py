03d25c8ab1da47ad0d98195a68b3c3ade8a813d5
Jimut123 <jimutbahanpal@yahoo.com>
2021-03-30 10:31:31 +0530

    added

--- models/drrmsan_022_conc.py
+++ models/drrmsan_022_conc.py
@@ -235,9 +235,7 @@ def proposed_attention_block_2d(ms_conv, res_block, filters):
     
     attn_outputs_mult = Activation('sigmoid')(multiply([output_ms_conv_res_block, output_ms_conv_res_block_rb]))
     attn_output_1 = Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(ZeroPadding2D(padding=(5,5))(UpSampling2D(size=(2, 2))(attn_outputs_mult)))
-
-    theta_x_rb = Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(theta_x_rb)
-    attn_output = multiply([attn_output_1, theta_x_rb])
+    
     return attn_output_1
 
 
